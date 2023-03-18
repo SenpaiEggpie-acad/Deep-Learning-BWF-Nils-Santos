81a55211e5071db683011980247ebcd78e7b4fe1
Nils <nbpsantos@mymail.mapua.edu.ph>
2023-03-19 00:10:02 +0800

    Added Chap 2 - Exercise 1-2

---
+++ Task3/Chap2-Exercise1-2.py
@@ -0,0 +1,10 @@
+#Exercise 2-1
+message = "Hello, this is for exercise 2-1."
+print (message)
+
+
+#Exercise 2-2
+message2 = "This is the first message."
+print(message2)
+message2 = "This is the second message."
+print(message2)
