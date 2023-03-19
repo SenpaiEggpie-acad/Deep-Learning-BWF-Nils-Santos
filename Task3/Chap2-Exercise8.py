b33934db7131bde8cd0da9178b3413a8f95ddc20
Nils <nbpsantos@mymail.mapua.edu.ph>
2023-03-19 08:23:41 +0800

    Added exercises 8 and 9

---
+++ Task3/Chap2-Exercise8.py
@@ -0,0 +1,7 @@
+#Made by Nils Santos
+#Four basic operations for number 8
+
+print(6+2)
+print(11-3)
+print(4*2)
+print(16/2)
---
+++ Task3/Chap2-Exercise9.py
@@ -0,0 +1,3 @@
+#Made by Nils Santos
+favorite_number = 120
+print("My favorite number is "+str(favorite_number)+"!")
