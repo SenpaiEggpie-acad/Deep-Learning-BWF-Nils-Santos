7e7258c60b4762b7e8789f9fd2444c8f5a7addac
Nils <nbpsantos@mymail.mapua.edu.ph>
2023-03-26 23:19:57 +0800

    Weekly Tasks done

---
+++ Task6/Chap6-Exercise3.py
@@ -0,0 +1,13 @@
+#Made by Nils Santos
+
+prog_words = {'append':"add something to a list",
+              'lower': "turn a string to lowercase",
+              'title': "turn a string to title case",
+              'del': "remove",
+              'if-elif': "a chain of conditional statements using if and elif"}
+
+print("append" + " - " + prog_words['append'])
+print("lower" + " - " +prog_words['lower'])
+print("title" + " - " +prog_words['title'])
+print("del" + " - " +prog_words['del'])
+print("if-elif" + " - " +prog_words['if-elif'])
