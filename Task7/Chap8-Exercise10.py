b488b7d93fecf8a1e696a7b5074621e5ef98a942
Nils <nbpsantos@mymail.mapua.edu.ph>
2023-04-17 22:47:39 +0800

    Task 18 Done

--- Task7/Chap8-Exercise10.py
+++ Task7/Chap8-Exercise10.py
@@ -2,9 +2,17 @@
 magician_list = ['Magic Man', 'Dr. Fate', 'Dr. Strange', 'Mr. M',
                  'Zatana']
 
+def show_magicians(magicians):
+    for magician in magicians:
+        print(magician)
+
 def make_great(magicians):
+    temp_list = []
     for magician in magicians:
         magician = ("the Great " + magician)
-        magicians.replace()
+        temp_list.append(magician)
+    magicians = temp_list.copy()
+    return magicians
 
-
+make_great(magician_list)
+show_magicians(magician_list)
